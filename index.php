21489b0faddff2abb215d76322f6158b1e6c1608
zayedutsho <zayedutsho@gmail.com>
2021-06-03 20:36:41 +0600

    folder created

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo"<h1>This is our first php class</h1>";
+  
+
+  $username="zayed";
+  $pass="1244df";
+  $email="zayedutsho@gmail.com";
+  $cgpa=2.4;
+  $age=23;
+
+  echo"Hello myself :".$username."<br> my password is:".$pass." <br>my mail adress is :".$email."<br>my cpa is".$cgpa."";
+
+
+    
+
+?>  
